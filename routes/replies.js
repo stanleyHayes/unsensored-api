d9ff08983921e15384f72e07990aac0ce765028a
Stanley Hayford <hayfordstanley@gmail.com>
2020-09-22 19:37:44 +0000

    reply routes added

--- routes/replies.js
+++ routes/replies.js
@@ -1,74 +1,13 @@
-const Reply = require('../models/reply');
+const express = require("express");
 
-exports.createReply = async (req, res) => {
-    try {
-        let reply = new Reply({
-            text: req.body.text,
-            author: req.user._id,
-            article: req.body.article,
-            comment: req.body.comment
-        });
+const {createReply, deleteReply, getReplies, getReply, updateReply} = require("../controllers/replies");
+const {auth} = require("../middleware/auth");
+const router = express.Router({mergeParams: true});
 
-        await reply.save();
-        reply = await Reply.findById(reply._id);
-        return res.status(201).json({data: reply});
-    }catch (e) {
-        return res.status(500).json({error: e.message});
-    }
-}
+router.post('/', auth, createReply);
+router.get('/', auth, getReplies);
+router.delete('/:id', auth, deleteReply);
+router.patch('/:id', auth, updateReply);
+router.get('/:id', getReply);
 
-exports.getReply = async (req, res) => {
-    try {
-        const reply = await Reply.findById(req.params.id);
-        if(!reply){
-            return res.status(400).json({error: 'comment not found'});
-        }
-        return res.status(200).json({data: reply});
-    }catch (e) {
-        return res.status(500).json({error: e.message});
-    }
-}
-
-exports.getReplies = async (req, res) => {
-    try {
-        const replies = await Reply.find({comment: req.body.comment, article: req.body.article});
-        return res.status(200).json({data: replies});
-    }catch (e) {
-        return res.status(500).json({error: e.message});
-    }
-}
-
-exports.updateReply = async (req, res) => {
-    try {
-        let reply = await Reply.findOne({author: req.user._id, _id: req.params.id});
-        if (!reply) {
-            return res.status(404).json({error: 'action not allowed'});
-        }
-        const allowedUpdates = ['text'];
-        const updates = Object.keys(req.body);
-        const isAllowed = updates.every(update => allowedUpdates.includes(update));
-        if (!isAllowed) {
-            return res.status(400).json({error: 'action not allowed'});
-        }
-        for (let key of updates) {
-            reply[key] = req.body[key];
-        }
-        await reply.save();
-        return res.status(200).json({data: reply});
-    }catch (e) {
-        return res.status(500).json({error: e.message});
-    }
-}
-
-exports.deleteReply = async (req, res) => {
-    try {
-        let reply = await Reply.findOne({author: req.user._id, _id: req.params.id});
-        if (!reply) {
-            return res.status(404).json({error: 'action not allowed'});
-        }
-        await reply.remove();
-        return res.status(200).json({data: reply});
-    }catch (e) {
-        return res.status(500).json({error: e.message});
-    }
-}
+module.exports = router;
